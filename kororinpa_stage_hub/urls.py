532787f987c5c67092ecb5cd3a9998d1be1fcb34
DigitalDetective47 <ninji2701@gmail.com>
2025-02-21 17:25:56 -0500

    add basic test page

--- kororinpa_stage_hub/urls.py
+++ kororinpa_stage_hub/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("user/", include("user_profile.site.urls"))
+    path("user/", include("user_profile.urls"))
 ]
