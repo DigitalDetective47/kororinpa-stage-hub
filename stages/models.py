12aeffa3b844387c50d4da791a7ce0185aa29ccb
DigitalDetective47 <ninji2701@gmail.com>
2025-02-21 18:47:04 -0500

    add track model

--- stages/models.py
+++ stages/models.py
@@ -1,3 +1,13 @@
+from django.core import validators
 from django.db import models
 
 # Create your models here.
+class Track(models.Model):
+    """One of the game's available music selections."""
+    __slots__ = ()
+
+    title = models.CharField(max_length=32)
+    ost_link = models.URLField(verbose_name="OST Link")
+
+    def __str__(self) -> str:
+        return self.title
