DigitalDetective47/kororinpa-stage-hub

commit c8a41fe054aee502d8043a401741caf128e96f22

rename Submission to Stage

views.py
@@ -17,11 +17,11 @@ from django.utils.text import slugify
 from koro import BinSlot
 
 from .forms import SearchStageForm, SubmitStageForm
-from .models import Submission, music_choices, music_ytids
+from .models import Stage, music_choices, music_ytids
 
 
 def view_stage(request: HttpRequest, pk: int) -> HttpResponse:
-    target: Final[Submission] = get_object_or_404(Submission, id=pk)
+    target: Final[Stage] = get_object_or_404(Stage, id=pk)
     return render(
         request,
         "kororinpa_stage_hub/index.html",
@@ -49,7 +49,7 @@ def view_stage(request: HttpRequest, pk: int) -> HttpResponse:
 
 @login_required
 def edit_stage(request: HttpRequest, pk: int) -> HttpResponse:
-    target: Final[Submission] = get_object_or_404(Submission, id=pk)
+    target: Final[Stage] = get_object_or_404(Stage, id=pk)
     if target.creator != request.user and not cast(User, request.user).has_perm(
         "kororinpa_stage_hub.change_submission"
     ):
@@ -71,7 +71,7 @@ def edit_stage(request: HttpRequest, pk: int) -> HttpResponse:
 
 @login_required
 def delete_stage(request: HttpRequest, pk: int) -> HttpResponse:
-    target: Final[Submission] = get_object_or_404(Submission, id=pk)
+    target: Final[Stage] = get_object_or_404(Stage, id=pk)
     if target.creator != request.user and not cast(User, request.user).has_perm(
         "kororinpa_stage_hub.delete_submission"
     ):
@@ -86,7 +86,7 @@ def delete_stage(request: HttpRequest, pk: int) -> HttpResponse:
 
 
 def download_stage(request: HttpRequest, pk: int) -> HttpResponse:
-    target: Final[Submission] = get_object_or_404(Submission, id=pk)
+    target: Final[Stage] = get_object_or_404(Stage, id=pk)
     target.stage_data.open("rb")
     content: Final[bytes] = target.stage_data.read()
     ret: HttpResponse
@@ -116,7 +116,7 @@ def submit_stage(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = SubmitStageForm(request.POST, request.FILES)
         if form.is_valid():
-            new: Final[Submission] = form.save(False)
+            new: Final[Stage] = form.save(False)
             new.creator = request.user
             new.save()
             ret: HttpResponseRedirect = HttpResponseRedirect(
@@ -143,7 +143,7 @@ def search_results_stage(request: HttpRequest) -> HttpResponse:
         return HttpResponseRedirect(
             reverse("kororinpa_stage_hub:search_stage", query=request.GET)
         )
-    query: QuerySet = Submission.objects.order_by(
+    query: QuerySet = Stage.objects.order_by(
         ("-" if form.cleaned_data["sort_direction"] == "desc" else "")
         + form.cleaned_data["sort"]
     )

admin.py
@@ -1,5 +1,5 @@
 from django.contrib.admin import site
 
-from .models import Submission
+from .models import Stage
 
-site.register(Submission)
+site.register(Stage)

forms.py
@@ -10,12 +10,12 @@ from django.forms import (
     RadioSelect,
 )
 
-from .models import Submission
+from .models import Stage
 
 
 class SubmitStageForm(ModelForm):
     class Meta:
-        model = Submission
+        model = Stage
         fields = ["name", "stage_data", "embed", "description", "music"]
 
 

migrations/0006_rename_submission_stage.py
@@ -1,0 +1,19 @@
+# Generated by Django 6.1.1 on 2026-09-14 19:20
+
+from django.conf import settings
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('kororinpa_stage_hub', '0005_remove_submission_modified_alter_submission_released_and_more'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.RenameModel(
+            old_name='Submission',
+            new_name='Stage',
+        ),
+    ]

models.py
@@ -38,7 +38,7 @@ with open("kororinpa_stage_hub/music.csv", newline="") as f:
     )
 
 
-class Submission(Model):
+class Stage(Model):
     name: CharField = CharField(max_length=255)
     stage_data: FileField = FileField()
     creator: ForeignKey = ForeignKey(User, on_delete=RESTRICT)
@@ -67,13 +67,13 @@ class Submission(Model):
         return reverse("kororinpa_stage_hub:view_stage", kwargs={"pk": self.pk})
 
 
-@receiver(pre_save, sender=Submission)
-def set_dates(sender: Any, instance: Submission, **kwargs: Any) -> None:
+@receiver(pre_save, sender=Stage)
+def set_dates(sender: Any, instance: Stage, **kwargs: Any) -> None:
     instance.updated = Now()
 
 
-@receiver(post_save, sender=Submission)
-def fix_xmls(sender: Any, instance: Submission, **kwargs: Any) -> None:
+@receiver(post_save, sender=Stage)
+def fix_xmls(sender: Any, instance: Stage, **kwargs: Any) -> None:
     instance.stage_data.open("rb")
     if instance.stage_data.read(1)[0]:
         instance.stage_data.open("rb")

commit 6a5a657b623b3fa80b75e23819725cf6732ae929

fix permission checking broken by c8a41fe

views.py
@@ -33,14 +33,14 @@ def view_stage(request: HttpRequest, pk: int) -> HttpResponse:
             and (
                 target.creator == request.user
                 or cast(User, request.user).has_perm(
-                    "kororinpa_stage_hub.change_submission"
+                    "kororinpa_stage_hub.change_stage"
                 )
             ),
             "delete_permission": request.user.is_authenticated
             and (
                 target.creator == request.user
                 or cast(User, request.user).has_perm(
-                    "kororinpa_stage_hub.delete_submission"
+                    "kororinpa_stage_hub.delete_stage"
                 )
             ),
         },
@@ -51,7 +51,7 @@ def view_stage(request: HttpRequest, pk: int) -> HttpResponse:
 def edit_stage(request: HttpRequest, pk: int) -> HttpResponse:
     target: Final[Stage] = get_object_or_404(Stage, id=pk)
     if target.creator != request.user and not cast(User, request.user).has_perm(
-        "kororinpa_stage_hub.change_submission"
+        "kororinpa_stage_hub.change_stage"
     ):
         return HttpResponseForbidden("You do not have permission to edit this stage")
     form: SubmitStageForm
@@ -73,7 +73,7 @@ def edit_stage(request: HttpRequest, pk: int) -> HttpResponse:
 def delete_stage(request: HttpRequest, pk: int) -> HttpResponse:
     target: Final[Stage] = get_object_or_404(Stage, id=pk)
     if target.creator != request.user and not cast(User, request.user).has_perm(
-        "kororinpa_stage_hub.delete_submission"
+        "kororinpa_stage_hub.delete_stage"
     ):
         return HttpResponseForbidden("You do not have permission to delete this stage")
     if request.method == "POST":